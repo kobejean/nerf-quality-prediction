d3c73f0e540a583c8fa33c3521b31c5a6d18620a
kobejean <s1292010@u-aizu.ac.jp>
2023-10-18 10:07:45 +0900

    update

--- nqp/configs/dataparser_configs.py
+++ nqp/configs/dataparser_configs.py
@@ -9,6 +9,8 @@ from __future__ import annotations
 from nerfstudio.plugins.registry_dataparser import DataParserSpecification
 from nqp.data.dataparsers.nqp_dataparser import NQPDataParserConfig
 from nqp.data.dataparsers.nqp_blender_dataparser import NQPBlenderDataParserConfig
+from nqp.data.dataparsers.nqp_minimal_dataparser import NQPMinimalDataParserConfig
 
 nqp_dataparser = DataParserSpecification(config=NQPDataParserConfig())
 nqp_blender_dataparser = DataParserSpecification(config=NQPBlenderDataParserConfig())
+nqp_minimal_dataparser = DataParserSpecification(config=NQPMinimalDataParserConfig())

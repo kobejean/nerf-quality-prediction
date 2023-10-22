c48e8971d718c67585cd882fef392bc405852177
kobejean <s1292010@u-aizu.ac.jp>
2023-10-22 18:11:52 +0900

    update

--- nqp/models/tensorf.py
+++ nqp/models/tensorf.py
@@ -44,6 +44,7 @@ from nerfstudio.utils import colormaps, colors, misc
 from nerfstudio.cameras.camera_optimizers import CameraOptimizer, CameraOptimizerConfig
 
 from nqp.fields.tensorf_field import NQPTensoRFField
+from nqp.model_components.renderers import FeaturesRenderer
 
 class NQPTensoRFModel(TensoRFModel):
 
@@ -105,6 +106,7 @@ class NQPTensoRFModel(TensoRFModel):
         self.renderer_rgb = RGBRenderer(background_color=colors.WHITE)
         self.renderer_accumulation = AccumulationRenderer()
         self.renderer_depth = DepthRenderer()
+        self.renderer_features = FeaturesRenderer()
 
         # losses
         self.rgb_loss = MSELoss()
@@ -152,6 +154,14 @@ class NQPTensoRFModel(TensoRFModel):
         accumulation = torch.clamp(accumulation, min=0)
 
         outputs = {"rgb": rgb, "accumulation": accumulation, "depth": depth}
+        
+        if not self.training:
+            features = self.renderer_features(
+                features=field_outputs_fine["features"],
+                weights=weights_fine,
+            )
+            outputs["features"] = features
+        
         return outputs
     
     def get_image_metrics_and_images(
@@ -169,7 +179,16 @@ class NQPTensoRFModel(TensoRFModel):
             far_plane=self.config.collider_params["far_plane"],
         )
 
+
         combined_rgb = torch.cat([image, rgb], dim=1)
+
+        # Switch images from [H, W, C] to [1, C, H, W] for metrics computations
+        image_flat = torch.moveaxis(image, -1, 0)[None, ...]
+        rgb_flat = torch.moveaxis(rgb, -1, 0)[None, ...]
+
+        psnr = self.psnr(image_flat, rgb_flat)
+        ssim = cast(torch.Tensor, self.ssim(image_flat, rgb_flat))
+        lpips = self.lpips(image_flat, rgb_flat)
 
         metrics_dict = {
             "psnr": float(psnr.item()),
@@ -177,4 +196,8 @@ class NQPTensoRFModel(TensoRFModel):
             "lpips": float(lpips.item()),
         }
         images_dict = {"img": combined_rgb, "rgb_gt": image, "rgb_pred": rgb, "accumulation": acc, "depth": depth}
+        
+        if not self.training:
+            images_dict["features"] = outputs["features"]
+
         return metrics_dict, images_dict
